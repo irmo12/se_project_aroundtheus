5b43ad3cf9401f3bfd051d7a0a01952c17c70c55
irmo12 <omriws@hushmail.com>
2022-04-04 22:32:58 +0300

    full featured form

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 let popupProfileEdit = document.querySelector(".popup");
 let buttonEditProfile = document.querySelector(".profile__edit-button");
-
-let fieldProfileName = document.querySelector(
-  ".profile-edit__name"
-);
-console.log(fieldProfileName.textContent);
-let fieldProfileAbout = document.querySelector(
-  ".profile-edit__about"
-);
+let buttonEditProfileClose = document.querySelector(".profile-edit__close");
+let formEditProfile = document.querySelector(".profile-edit");
+let fieldProfileName = document.querySelector(".profile-edit__name");
+let fieldProfileAbout = document.querySelector(".profile-edit__about");
 
 function handleEditProfileBtn() {
   let profileName = document.querySelector(".profile__user-name").textContent;
   let profileAbout = document.querySelector(".profile__user-about").textContent;
-console.log(profileName);
+
   popupProfileEdit.classList.add("popup__active");
 
   fieldProfileName.value = profileName;
   fieldProfileAbout.value = profileAbout;
+}
 
+function handleEditProfileClose() {
+  popupProfileEdit.classList.remove("popup__active");
+}
+
+function handleEditProfileSave(evt) {
+  evt.preventDefault();
+
+  document.querySelector(".profile__user-name").textContent =
+    fieldProfileName.value;
+  document.querySelector(".profile__user-about").textContent =
+    fieldProfileAbout.value;
+
+  popupProfileEdit.classList.remove("popup__active");
 }
 
 buttonEditProfile.addEventListener("click", handleEditProfileBtn);
+buttonEditProfileClose.addEventListener("click", handleEditProfileClose);
+formEditProfile.addEventListener("submit", handleEditProfileSave);
