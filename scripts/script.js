ec81290f38321b1d0926a3c4e90a245907b4172e
irmo12 <omriws@hushmail.com>
2022-04-18 15:22:14 +0300

    refactor to populate gallery from script (proj step 1)

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,61 @@ let buttonEditProfile = document.querySelector(".profile__edit-button");
 let buttonEditProfileClose = document.querySelector(".popup__container-close");
 let formEditProfile = document.querySelector(".profile-edit");
 let fieldProfileName = document.querySelector(".profile-edit__field_user_name");
-let fieldProfileAbout = document.querySelector(".profile-edit__field_user_about");
+let fieldProfileAbout = document.querySelector(
+  ".profile-edit__field_user_about"
+);
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    src: "./images/kirill-pershin-1088404-unsplash.png",
+    alt: "Yosemite el capitan",
+  },
+  {
+    name: "Lake Louise",
+    src: "./images/kirill-pershin-1404681-unsplash.png",
+    alt: "Lake Louise",
+  },
+  {
+    name: "Bald Mountains",
+    src: "./images/kirill-pershin-1556355-unsplash.png",
+    alt: "Bald Mountains",
+  },
+  {
+    name: "Latemar",
+    src: "./images/kirill-pershin-1404681-unsplash-1.png",
+    alt: "Latemar",
+  },
+  {
+    name: "Vanoise National Park",
+    src: "./images/kirill-pershin-1556355-unsplash-1.png",
+    alt: "Vanoise National Park",
+  },
+  {
+    name: "Lago di Braies",
+    src: "./images/kirill-pershin-1088404-unsplash-1.png",
+    alt: "Lago di Braies",
+  },
+];
+const gallery = document.querySelector('.gallery');
+const cardTemplate = document.querySelector('#card').content;
+
+function populateInitial() {
+  initialCards.forEach(function (card){
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImg = cardElement.querySelector('.card__img');
+    const cardTitle = cardElement.querySelector('.card__caption');
+    
+    cardImg.setAttribute('src', card.src);
+    cardImg.setAttribute('alt', card.alt);
+    cardTitle.textContent = card.name;
+    
+    gallery.append(cardElement);
+    
+  })
+}
+populateInitial(initialCards);
+
+
 
 function handleEditProfileBtn() {
   let profileName = document.querySelector(".profile__user-name").textContent;
