dacf440c39196f41e054c000729b2ef9afa173c7
irmo12 <omriws@hushmail.com>
2022-04-21 20:36:17 +0300

    add image popout and cose button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 let buttonEditProfile = document.querySelector(".profile__edit-button");
 let buttonAddCard = document.querySelector(".profile__add-btn");
 let popup = document.querySelector(".popup");
+let popupCloseBtn = popup.querySelector(".popup__container-close");
 let buttonPopupClose = popup.querySelector(".popup__container-close");
+let imgTemplate = document.querySelector("#imgPopout").content;
+let imgElement = imgTemplate.querySelector(".img-popout").cloneNode(true);
+let gallery = document.querySelector(".gallery");
+let cardTemplate = document.querySelector("#card").content;
+let popupTemplate = document.querySelector("#popup-template").content;
+let popupElement = popupTemplate
+  .querySelector(".popup__container")
+  .cloneNode(true);
+const mediaQuery = window.matchMedia("(max-width: 796px)");
 
 const initialCards = [
   {
@@ -11,12 +21,12 @@ const initialCards = [
   },
   {
     name: "Lake Louise",
-    src: "./images/kirill-pershin-1404681-unsplash.png",
+    src: "./images/unsplashtst/wil-stewart-T26KCgCPsCI-unsplash.jpg",
     alt: "Lake Louise",
   },
   {
     name: "Bald Mountains",
-    src: "./images/kirill-pershin-1556355-unsplash.png",
+    src: "./images/unsplashtst/craig-strahorn--UmCb7gAAUI-unsplash.jpg",
     alt: "Bald Mountains",
   },
   {
@@ -35,27 +45,25 @@ const initialCards = [
     alt: "Lago di Braies",
   },
 ];
-const gallery = document.querySelector(".gallery");
-const cardTemplate = document.querySelector("#card").content;
 
 function populate(cardsArr = initialCards) {
   cardsArr.forEach(function (card) {
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
     const cardImg = cardElement.querySelector(".card__img");
     const cardTitle = cardElement.querySelector(".card__caption");
     const btnLike = cardElement.querySelector(".card__like-btn");
-    const btnTrash = cardElement.querySelector('.card__trash');
+    const btnTrash = cardElement.querySelector(".card__trash");
 
     cardImg.setAttribute("src", card.src);
     cardImg.setAttribute("alt", card.alt);
     cardTitle.textContent = card.name;
 
-    btnLike.addEventListener("click", function(evt) {
+    btnLike.addEventListener("click", function (evt) {
       evt.target.classList.toggle("card__like-btn_active");
     });
 
-    btnTrash.addEventListener("click", function(evt) {
-      evt.target.closest('.card').remove();
+    btnTrash.addEventListener("click", function (evt) {
+      evt.target.closest(".card").remove();
     });
 
     cardImg.addEventListener("click", hanldeImgClick);
@@ -69,10 +77,6 @@ function handleEditProfileBtn() {
   let profileName = document.querySelector(".profile__user-name").textContent;
   let profileAbout = document.querySelector(".profile__user-about").textContent;
 
-  popupTemplate = document.querySelector("#popup-template").content;
-  popupElement = popupTemplate
-    .querySelector(".popup__container")
-    .cloneNode(true);
   popup.classList.add("popup_active");
 
   let formEditProfile = popupElement.querySelector(".profile-edit");
@@ -87,7 +91,15 @@ function handleEditProfileBtn() {
   fieldProfileAbout.value = profileAbout;
 
   popup.prepend(popupElement);
-
+  let formWidth = popupElement.offsetWidth;
+  if (mediaQuery.matches) {
+    buttonPopupClose.setAttribute("style", "margin: 0 0 20px 80vw;");
+  } else {
+    buttonPopupClose.setAttribute(
+      "style",
+      `margin-left: calc(${formWidth}px + 36px);`
+    );
+  }
   function handleEditProfileSave(evt) {
     evt.preventDefault();
 
@@ -117,7 +129,7 @@ function handleAddCard() {
   popup.classList.add("popup_active");
 
   let formEditProfile = popupElement.querySelector(".profile-edit");
-  let formHeading = popupElement.querySelector('.profile-edit__heading')
+  let formHeading = popupElement.querySelector(".profile-edit__heading");
   let fieldProfileName = popupElement.querySelector(
     ".profile-edit__field_user_name"
   );
@@ -130,6 +142,15 @@ function handleAddCard() {
   fieldProfileAbout.value = "Image link";
 
   popup.prepend(popupElement);
+  let formWidth = popupElement.offsetWidth;
+  if (mediaQuery.matches) {
+    buttonPopupClose.setAttribute("style", "margin: 0 0 20px 80vw;");
+  } else {
+    buttonPopupClose.setAttribute(
+      "style",
+      `margin-left: calc(${formWidth}px + 36px);`
+    );
+  }
 
   function handleEditProfileSave(evt) {
     evt.preventDefault();
@@ -147,21 +168,32 @@ function handleAddCard() {
   formEditProfile.addEventListener("submit", handleEditProfileSave);
 }
 
+function hanldeImgClick(evt) {
+  const img = imgElement.querySelector(".img-popout__img");
+  const cap = imgElement.querySelector(".img-popout__caption");
 
-function handleEditProfileClose() {
-  popup.classList.remove("popup_active");
-  popupElement.remove();
+  img.setAttribute("src", evt.target.getAttribute("src"));
+  cap.setAttribute("alt", evt.target.getAttribute("alt"));
+
+  popup.classList.add("popup_active");
+  popup.prepend(imgElement);
+  let width = img.offsetWidth;
+  buttonPopupClose.setAttribute(
+    "style",
+    `margin-left: calc(${width}px + 36px);`
+  );
 }
 
-function hanldeImgClick(evt) {
-  const
-  const
-
-  popup.classList.add('popup_active');
-
+function handleEditProfileClose(evt) {
+  popup.classList.remove("popup_active");
+  if (!!popupElement) {
+    popupElement.remove();
+  }
+  if (!!imgElement) {
+    imgElement.remove();
+  }
 }
 
 buttonEditProfile.addEventListener("click", handleEditProfileBtn);
 buttonAddCard.addEventListener("click", handleAddCard);
 buttonPopupClose.addEventListener("click", handleEditProfileClose);
-
